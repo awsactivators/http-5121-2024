0270573226981d607762229a83341f3c3ba884aa
Genevieve <awavieve@gmail.com>
2024-11-07 18:56:55 -0500

    added week 9 files

---
+++ HTML-Assignment3-GenevieveAwa/js/main.js
@@ -0,0 +1,11 @@
+// document.querySelectorAll('nav a').forEach(anchor => {
+//   anchor.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     const offset = 700; // Adjust based on nav bar height
+//     window.scrollTo({
+//       top: target.offsetTop - offset,
+//       behavior: 'smooth'
+//     });
+//   });
+// });
